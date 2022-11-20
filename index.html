e0a87fc8600e89caeaed832c0beea78f16d28442
pudim11 <78266234+pudim11@users.noreply.github.com>
2022-11-19 23:24:12 -0300

    sim

--- index.html
+++ index.html
@@ -1,3 +1,7 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
--- script/scriptSAUDE.js
+++ script/scriptSAUDE.js
@@ -1,38 +1,3 @@
-/*
-
-const BtnDsts = document.getElementById("btn-DSTs")
-const TextDsts = document.getElementById("text-DSts")
-
-
-
-
-function BtnActiveDsts(){
-   document.getElementById("text-DSts").style.color = "red";
-
-}
-
-function BtnActiveDepressao(){
-    document.getElementById("text-depressao").style.color = "blue";
-}
-function BtnActiveDoencas(){
-    document.getElementById("text-doencas").style.color = "green";
-}*/
-
-const BtnDsts = document.querySelector("#btn-DSTs")
-const TextDsts = document.querySelector("#text-DSts")
-const AreaTextDepre = document.querySelector("#area-text-depresao")
-const AreaTextDsts = document.querySelector("#area-text-dsts")
-
-
-
-BtnDsts.addEventListener('click', () => {
-    AreaTextDsts.classList.toggle('animar');
-  })
-
-
-function animando(){
-    TextDsts.classList.toggle("animar");
-}
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
